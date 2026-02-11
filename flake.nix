7897d9f263d6d994b3e73a772a5fe5bb97897f31
xox <xox@nix.os>
2026-02-12 05:09:36 +0600

    mango config

--- flake.nix
+++ flake.nix
@@ -1,20 +1,51 @@
 {
-  description = "NixOS with MangoWC";
+  description = "NixOS with MangoWC + Home Manager";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     mango = {
       url = "github:DreamMaoMao/mango";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, mango, ... }: {
+  outputs = { self, nixpkgs, mango, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in
+  {
     nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
+
       modules = [
         mango.nixosModules.mango
+        home-manager.nixosModules.home-manager
+
         ./hosts/nixos/configuration.nix
+
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.xox = { pkgs, ... }: {
+            home.stateVersion = "23.11"; # change if needed
+
+            # Install Mango for user
+            home.packages = [
+              pkgs.mangowc
+            ];
+
+            # Provide Mango config in ~/.config
+            home.file.".config/mango/config.conf".source =
+              "${pkgs.mangowc}/etc/mango/config.conf";
+          };
+        }
       ];
     };
   };
