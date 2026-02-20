4d8437738ec19e988f438cc315f01969beb03d51
xox <xox@fake.me>
2026-02-21 02:25:03 +0600

    startship config with home-manager

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 {
-    programs.starship = {
-        enable = true;
-        enableFishIntegration = true;
-    };
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = (builtins.fromTOML (builtins.readFile ./starship.toml));
+  };
 }
-
